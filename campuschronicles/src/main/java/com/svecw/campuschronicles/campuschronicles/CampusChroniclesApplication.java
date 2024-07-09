8e05639defab638bb2cc4000373f14f3569d1afd
Poojitha <22b01a12c3@svecw.edu.in>
2024-07-09 09:12:20 +0530

    Intial commit for a Social Platform- Campus Chronicles

---
+++ campuschronicles/src/main/java/com/svecw/campuschronicles/campuschronicles/CampusChroniclesApplication.java
@@ -0,0 +1,13 @@
+package com.svecw.campuschronicles.campuschronicles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusChroniclesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusChroniclesApplication.class, args);
+	}
+
+}
---
+++ campuschronicles/src/test/java/com/svecw/campuschronicles/campuschronicles/CampusChroniclesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svecw.campuschronicles.campuschronicles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusChroniclesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
